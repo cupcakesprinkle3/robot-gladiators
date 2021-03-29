ca437a0e9603a8570d5c5911bf02f94b68fabe55
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-03-28 19:43:21 -0700

    did some stuff in modules

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
+window.prompt("What is your robot's name?");
+
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
